9e0a78dcdb6563625081a3c1086df93ac9b92455
Diane Magana <dianemagana@outlook.com>
2020-09-30 22:42:55 -0700

    Trying again

--- Develop/server.js
+++ Develop/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static(_dirname + '/public'));
 
 app.get("/notes", (req,res)=>{
-    res.sendFile(path.join(_dirname,"public/notes.html"));
+    res.sendFile(path.join(_dirname,"../public/notes.html"));
 });
 app.get("/api/notes",(req,res)=>{
     readFileAsync("./db/db.json", "utf8")
@@ -23,7 +23,7 @@ app.get("/api/notes",(req,res)=>{
 });
 
 app.get("*", (req,res)=>{
-    res.sendFile(path.join(_dirname,"public/index.html"));
+    res.sendFile(path.join(_dirname,"../public/index.html"));
 });
 
 app.post("/api/notes",(req,res)=>{
