2346dd2319498f97104261fb7a45a1061f16fd5e
Diane Magana <dianemagana@outlook.com>
2020-09-30 23:08:48 -0700

    Update

--- Develop/server.js
+++ Develop/server.js
@@ -7,34 +7,38 @@ const PORT = process.env.PORT || 3000;
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
-app.use(express.static(_dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/notes", (req,res)=>{
-    res.sendFile(path.join(_dirname,"../public/notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
-app.get("/api/notes",(req,res)=>{
+app.get("/api/notes", (req,res)=>{
+
     readFileAsync("./db/db.json", "utf8")
     .then((result, err)=>{
-        if(err) console.log(err);
-        return res.json(JSON.parse(result));
-    });
+        if(err) console.log(err);       
+        return res.json(JSON.parse(result));       
+    });     
 });
 
 app.get("*", (req,res)=>{
-    res.sendFile(path.join(_dirname,"../public/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.post("/api/notes",(req,res)=>{
+app.post("/api/notes", (req,res)=>{
     let newNote = req.body;
-
+   
     readFileAsync("./db/db.json", "utf8")
     .then((result, err)=>{
-        return Promise.resolve(JSON.parse(result));
+        if(err) console.log(err);
+        return Promise.resolve(JSON.parse(result));               
     })
     .then(data =>{
+     
         newNote.id = getLastIndex(data) + 1;
+        
         (data.length > 0)? data.push(newNote):data = [newNote];
         return Promise.resolve(data);
     })
@@ -48,28 +52,32 @@ app.post("/api/notes",(req,res)=>{
 });
 
 app.delete('/api/notes/:id', (req,res)=>{
+    
     let id = req.params.id;
+      
     readFileAsync("./db/db.json", "utf8")
     .then((result, err)=>{
         if(err) console.log(err);
-        return Promise.resolve(JSON.parse(result));
+        return Promise.resolve(JSON.parse(result));               
     })
     .then(data =>{
-        data.splice(data.indexOf(data.find(element => element.id)),1);
+             
+        data.splice(data.indexOf(data.find(element => element.id == id)),1);
         return Promise.resolve(data);
     })
     .then(data =>{
+        
         writeFileAsync("./db/db.json", JSON.stringify(data));
-        res.send("ok");
+        res.send("OK");
     })
     .catch(err =>{
         if(err) throw err;
     });
 });
-app.use(funcation (req, res, next){
-    res.status(404).send("Error! Can't Find That")
-})
-app.listen(PORT, function(){
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+  })
+  app.listen(PORT, function(){
     console.log(`Listening on PORT ${PORT}`);
 });
 function getLastIndex(data){
